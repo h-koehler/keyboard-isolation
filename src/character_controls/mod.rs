b969e8d568db72cd1d3d01fb9b7dfad8b13be23d
DanielStebbins <danrstebb@gmail.com>
2026-02-07 16:26:02 -0500

    Adds 8-way flashlight rotation

--- src/character_controls/mod.rs
+++ src/character_controls/mod.rs
@@ -9,7 +9,6 @@ use bevy_lit::prelude::*;
 
 const DEBUG_BRIGHTNESS: bool = false;
 const MOVE_SPEED: f32 = 200.0;
-const VELOCITY_CHANGE: f32 = 1.0;
 const PLAYER_ASS_PATH: &str = "player_up.png";
 const PLAYER_SIZE: Option<Vec2> = Some(Vec2::new(64.0, 64.0));
 const ROOM_INSET: f32 = 4.0;
@@ -28,27 +27,21 @@ fn player_input(
 ) {
     let (mut trans, mut char_vel) = q_player.single_mut().expect("No Player Object");
     let mut dir = Vec2::ZERO;
-
     if inputs.pressed(KeyCode::KeyA) {
-        dir.x -= VELOCITY_CHANGE;
-        trans.rotation = Quat::from_axis_angle(Vec3::Z, PI / 2.0);
+        dir.x -= 1.0;
     }
     if inputs.pressed(KeyCode::KeyD) {
-        dir.x += VELOCITY_CHANGE;
-        trans.rotation = Quat::from_axis_angle(Vec3::Z, -PI / 2.0);
+        dir.x += 1.0;
     }
     if inputs.pressed(KeyCode::KeyW) {
-        dir.y += VELOCITY_CHANGE;
-        trans.rotation = Quat::from_axis_angle(Vec3::Z, 0.0);
+        dir.y += 1.0;
     }
     if inputs.pressed(KeyCode::KeyS) {
-        dir.y -= VELOCITY_CHANGE;
-        trans.rotation = Quat::from_axis_angle(Vec3::Z, PI);
+        dir.y -= 1.0;
     }
-
-    char_vel.linear_velocity = char_vel
-        .linear_velocity
-        .lerp(dir.normalize_or_zero() * MOVE_SPEED, 0.5);
+    dir = dir.normalize_or_zero();
+    trans.rotation = Quat::from_axis_angle(Vec3::Z, Vec2::X.angle_to(dir));
+    char_vel.linear_velocity = char_vel.linear_velocity.lerp(dir * MOVE_SPEED, 0.5);
 }
 
 fn apply_velocity(
@@ -122,8 +115,6 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
                     outer_angle: 25.0,
                     ..default()
                 },
-                Transform::from_xyz(0.0, 0.0, 0.0)
-                    .with_rotation(Quat::from_rotation_z(90_f32.to_radians())),
             ));
         });
 }
