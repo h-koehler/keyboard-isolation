4e33d94db04abc5a2975b97ac68a96c2443ba582
AnthonyTornetta <cornchipgonecodin@gmail.com>
2026-02-07 17:36:25 -0500

    added clamp

--- src/character_controls/flashlight.rs
+++ src/character_controls/flashlight.rs
@@ -20,9 +20,10 @@ fn update_flashlight(
     let enabled = inputs.pressed(KeyCode::Space);
 
     for (ent, mut flashlight, mut spotlight) in q_flashlight.iter_mut() {
-        if enabled {
+        if enabled && flashlight.max_charge > 0.0 {
             flashlight.max_charge -= time.delta_secs();
             spotlight.intensity = 1.0;
+            flashlight.max_charge = flashlight.max_charge.max(0.0);
             commands.entity(ent).insert(FlashlightActive);
         } else {
             spotlight.intensity = 0.0;
@@ -30,6 +31,23 @@ fn update_flashlight(
         }
     }
 }
+
+// fn add_flashlight(mut commands: Commands) {
+//     // commands.spawn((
+//     //     Node {
+//     //         top: Val::Px(50.0),
+//     //         right: Val::Px(50.0),
+//     //         ..Default::default()
+//     //     }
+//     // )).with_children(|p| {
+//     //     p.spawn((
+//     //
+//     //         )})
+// }
+//
+// fn update_flashlight_battery(q_bat_node: Query<&mut Node, With<BatteryAmount>) {
+//
+// }
 
 pub(super) fn register(app: &mut App) {
     app.add_systems(Update, update_flashlight);
