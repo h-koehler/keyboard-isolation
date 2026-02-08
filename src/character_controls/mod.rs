9ec92bf6b9da5d33d97534b861aac93527f7bd53
AnthonyTornetta <cornchipgonecodin@gmail.com>
2026-02-07 22:02:08 -0500

    Checkpoints restore flashlight

--- src/character_controls/mod.rs
+++ src/character_controls/mod.rs
@@ -1,5 +1,6 @@
 use crate::{
     character_controls::flashlight::Flashlight,
+    dialog::DialogOnClose,
     light::{CheckInLight, IgnoreInLightCheckLight},
     room::Movable,
 };
@@ -167,6 +168,7 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands
         .spawn((
             Name::new("Character"),
+            DialogOnClose("It's amazing I survived the crash...".into()),
             Character {
                 health: STARTING_HEALTH,
             },
